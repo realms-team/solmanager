8ecb01c3f0bfa6d8f6933a234ef5f5eaf08691e6
Keoma Brun <keoma.brun@inria.fr>
2016-04-10 11:14:34 -0300

    adding scripts to create ssh tunnel

---
+++ autossh/autossh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( pwd )"
+. ${DIR}/autossh.conf.sh
+
+
+NLINES=`ps aux | grep "$TUNNEL" | wc -l`
+
+if [ $NLINES -eq 1 ]
+then
+	ssh -NTR ${TUNNEL} -p ${REMOTEPORT} ${REMOTEUSER}@${REMOTEHOST}
+fi
+exit 0
