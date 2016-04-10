942b3b30ea1a3447e8c54f81ec20792216f21e0d
Keoma Brun <keoma.brun@inria.fr>
2016-04-10 11:32:46 -0300

    fixing path issue

--- autossh/autossh.sh
+++ autossh/autossh.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-DIR="$( pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/autossh.conf.sh
 
-
-NLINES=`ps aux | grep "$TUNNEL" | wc -l`
-
-if [ $NLINES -eq 1 ]
-then
-	ssh -NTR ${TUNNEL} -p ${REMOTEPORT} ${REMOTEUSER}@${REMOTEHOST}
-fi
-exit 0
+ssh -NTR ${TUNNEL} -p ${REMOTEPORT} ${REMOTEUSER}@${REMOTEHOST}
