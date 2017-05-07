3ef73a9a5edc9fbd0d420c84558ce547b1170325
Keoma Brun <keoma.brun@inria.fr>
2017-05-07 17:18:00 +0200

    #61 fixed

--- solmanager.py
+++ solmanager.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import division # using python3 divison to avoid truncation
+
 #============================ adjust path =====================================
 
 import sys
