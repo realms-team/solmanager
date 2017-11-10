c8248398081c8551f985fe35c33d289bbb07029d
Keoma Brun <keoma.brun@inria.fr>
2017-11-10 10:44:03 +0100

    #129 adding websocket to init file

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -1,9 +1,4 @@
 from connectors import connector
 from connectors import connector_https
 from connectors import connector_file
-import logging.config
-
-#============================ logging =========================================
-
-logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
-log = logging.getLogger("connector")
+from connectors import connector_websocket
